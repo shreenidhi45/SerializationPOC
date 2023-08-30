0e1e38020bd99c3e189144b1c4e0fb8c00458885
shreenidhi45 <75909459+shreenidhi45@users.noreply.github.com>
2023-08-30 23:50:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+
+namespace SerialDesrializePOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string xmlFilePath = ConfigurationManager.AppSettings["XmlFilePath"];
+                
+
+                // XML Serialization
+                Employee empXml = new Employee();
+                empXml.Id = 102;
+                empXml.Name = "SHREE";
+
+                XmlSerialization.Serialize(empXml, xmlFilePath);
+                Employee empXmlDeserialized = XmlSerialization.Deserialize(xmlFilePath);
+                Console.WriteLine("Deserialized XML: {0}, {1}", empXmlDeserialized.Id, empXmlDeserialized.Name);
+            }
+        }
+
+    }
