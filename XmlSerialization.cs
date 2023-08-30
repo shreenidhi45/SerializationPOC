f31aeb8082622d01d6fe7b35cc980755c877b777
shreenidhi45 <75909459+shreenidhi45@users.noreply.github.com>
2023-08-30 23:51:46 +0530

    Add files via upload

---
+++ XmlSerialization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerialDesrializePOC
+{
+    internal class XmlSerialization
+    {
+        public static void Serialize(Employee employee, string filePath)
+        {
+            using (FileStream stream = File.Create(filePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+                serializer.Serialize(stream, employee);
+            }
+        }
+
+        public static Employee Deserialize(string filePath)
+        {
+            using (FileStream stream = File.OpenRead(filePath))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(Employee));
+                return (Employee)deserializer.Deserialize(stream);
+            }
+        }
+    }
+}
